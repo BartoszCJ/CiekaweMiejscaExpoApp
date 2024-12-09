55a923edc55e3c33df5189f7e20c46dc8730bf42
BartoszCoJest <bartoszcojest@gmail.com>
2024-12-09 02:54:51 +0100

    Added more data

--- constants/index.ts
+++ constants/index.ts
@@ -4,7 +4,6 @@ import backArrow from "@/assets/icons/back-arrow.png";
 import chat from "@/assets/icons/chat.png";
 import checkmark from "@/assets/icons/check.png";
 import close from "@/assets/icons/close.png";
-import dollar from "@/assets/icons/dollar.png";
 import email from "@/assets/icons/email.png";
 import eyecross from "@/assets/icons/eyecross.png";
 import google from "@/assets/icons/google.png";
@@ -12,22 +11,18 @@ import home from "@/assets/icons/home.png";
 import list from "@/assets/icons/list.png";
 import lock from "@/assets/icons/lock.png";
 import map from "@/assets/icons/map.png";
-import marker from "@/assets/icons/marker.png";
 import out from "@/assets/icons/out.png";
 import person from "@/assets/icons/person.png";
 import pin from "@/assets/icons/pin.png";
 import point from "@/assets/icons/point.png";
 import profile from "@/assets/icons/profile.png";
 import search from "@/assets/icons/search.png";
-import selectedMarker from "@/assets/icons/selected-marker.png";
 import star from "@/assets/icons/star.png";
 import target from "@/assets/icons/target.png";
 import to from "@/assets/icons/to.png";
 import check from "@/assets/images/check.png";
-import getStarted from "@/assets/images/get-started.png";
 import message from "@/assets/images/message.png";
 import noResult from "@/assets/images/no-result.png";
-import signUpCar from "@/assets/images/signup-car.png";
 import welcome1 from "@/assets/images/welcome1.jpg";
 import welcome2 from "@/assets/images/welcome2.jpg";
 import welcome3 from "@/assets/images/welcome3.jpg";
@@ -38,8 +33,6 @@ export const zdjecia = {
   rejestracja,
   welcome2,
   welcome3,
-  getStarted,
-  signUpCar,
   check,
   noResult,
   message,
@@ -52,7 +45,6 @@ export const icons = {
   chat,
   checkmark,
   close,
-  dollar,
   email,
   eyecross,
   google,
@@ -60,14 +52,12 @@ export const icons = {
   list,
   lock,
   map,
-  marker,
   out,
   person,
   pin,
   point,
   profile,
   search,
-  selectedMarker,
   star,
   target,
   to,
